a9c1d93ffa7a5cad9832c37271febddd42692b4b
Valery Akhrymchuk <valeryakhrymchuk@gmail.com>
2025-02-23 13:50:02 +0300

    Send error message

--- src/main/kotlin/org/kor/portal/service/tm/TmBot.kt
+++ src/main/kotlin/org/kor/portal/service/tm/TmBot.kt
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Service
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.objects.Update
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException
 
@@ -28,9 +29,9 @@ class TmBot(
             }
         } catch (e: Exception) {
             logger.error("Failed to process telegram update", e)
+            sendErrorMessage(update, e)
         }
     }
-
 
     fun sendMessageToTelegram(botApiMethod: BotApiMethod<*>) {
         try {
@@ -40,6 +41,17 @@ class TmBot(
         } catch (e: TelegramApiException) {
             logger.error("Telegram Api Exception: {}", e.message, e)
             throw RuntimeException("Failed to send message to telegram", e)
+        }
+    }
+
+
+    private fun sendErrorMessage(update: Update, e: Exception) {
+        try {
+            val chatId = update.message.chatId.toString()
+            val message = SendMessage().create(chatId, "Произошла ошибка: ${e.message}")
+            sendMessageToTelegram(message)
+        } catch (e: Exception) {
+            logger.error("Failed to send telegram message with error", e)
         }
     }
 
